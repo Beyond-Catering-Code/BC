47b36c28c5947179d6a2c953c7f9c33ae24203d0
Mohammad <mohammad@ltv.so>
2025-07-30 15:45:41 +0400

    changes to GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -8,12 +8,23 @@ import Script from 'next/script';
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      {/* Load GTM script once */}
+      {/* Define window.dataLayer BEFORE GTM loads */}
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            event: 'gtm.js',
+            'gtm.start': new Date().getTime()
+          });
+        `}
+      </Script>
+
+      {/* Load GTM script */}
       <Script
         src="https://www.googletagmanager.com/gtm.js?id=GTM-PCX4MJSM"
         strategy="afterInteractive"
       />
-      
+
       <Navbar />
       <main>
         <Component {...pageProps} />
