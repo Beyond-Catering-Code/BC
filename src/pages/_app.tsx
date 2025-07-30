bf14bdc6e3eeaabfbc8e450a37c29c15eaeca60e
Mohammad <mohammad@ltv.so>
2025-07-30 15:25:44 +0400

    Made changes to GTM

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,17 +1,33 @@
-import '@/styles/globals.css'; 
-import '@fontsource/playfair-display/600.css'; // weight 600
+import '@/styles/globals.css';
+import '@fontsource/playfair-display/600.css';
 import type { AppProps } from 'next/app';
-import Navbar from '@/components/Navbar';  // Import Navbar
-import Footer from '@/components/Footer';  // Import Footer
+import Navbar from '@/components/Navbar';
+import Footer from '@/components/Footer';
+import Script from 'next/script';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <>
-      <Navbar />  {/* Global Navbar */}
+      {/* GTM Script */}
+      <Script id="gtm-init" strategy="afterInteractive">
+        {`
+          window.dataLayer = window.dataLayer || [];
+          window.dataLayer.push({
+            event: 'gtm.js',
+            'gtm.start': new Date().getTime()
+          });
+        `}
+      </Script>
+      <Script
+        src="https://www.googletagmanager.com/gtm.js?id=GTM-PCX4MJSM"
+        strategy="afterInteractive"
+      />
+
+      <Navbar />
       <main>
-        <Component {...pageProps} />  {/* Page-specific content */}
+        <Component {...pageProps} />
       </main>
-      <Footer />  {/* Global Footer */}
+      <Footer />
     </>
   );
 }
